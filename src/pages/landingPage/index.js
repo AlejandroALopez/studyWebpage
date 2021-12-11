f0ebf3f444aa53ce551cc15cbb3fd2c8e25c2222
alejandro <lopez.alejandro@dali.dartmouth.edu>
2021-12-11 14:47:48 -0500

    resuming project

--- src/pages/landingPage/index.js
+++ src/pages/landingPage/index.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 import './style.scss';
 
 // import { fetchConstants } from '../actions/constantsActions';
 
-class LandingPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // topics: 0,
-    };
-  }
-
-  render() {
-    return (
-      <div className="landingPage-container">
-        <div className="landingPage-header-container">
-          <div className="landingPage-header-title">This is the landing page!</div>
-        </div>
+export default function LandingPage(props) {
+  return (
+    <div className="landingPage-container">
+      <div className="landingPage-header-container">
+        <div>Home</div>
+        <div>Portfolio</div>
+        <div>Libraries</div>
+        <div>Resources</div>
+        <div>More</div>
       </div>
-    );
-  }
-}
 
-export default connect(null, {
-})(LandingPage);
+      <div>Welcome to my website!</div>
+      <div>
+        About me
+      </div>
+    </div>
+  );
+}
