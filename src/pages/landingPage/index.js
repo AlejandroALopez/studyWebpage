b98727e610e33cd4e57d47dd35a6b3e27d55706c
alejandro <lopez.alejandro@dali.dartmouth.edu>
2021-12-11 15:46:58 -0500

    home page and colors for other slides

--- src/pages/landingPage/index.js
+++ src/pages/landingPage/index.js
@@ -7,16 +7,43 @@ import './style.scss';
 export default function LandingPage(props) {
   return (
     <div className="landingPage-container">
-      <div className="landingPage-header-container">
-        <div>Home</div>
-        <div>Portfolio</div>
-        <div>Libraries</div>
-        <div>Resources</div>
-        <div>More</div>
+      <div className="landingPage-header">
+        <div className="landingPage-header-name">Alejandro Lopez</div>
+        <div className="landingPage-header-navigation">
+          <div>HOME</div>
+          <div>ABOUT ME</div>
+          <div>SKILLS</div>
+          <div>WORK</div>
+          <div>INTERESTS</div>
+          <div>CONTACT</div>
+        </div>
       </div>
-
-      <div>Welcome to my website!</div>
-      <div>
+      <div className="homepage">
+        <p className="intro">
+          Hello! I am
+          {' '}
+          <span>Alejandro Lopez</span>
+          , and welcome to my personal website!
+        </p>
+        <p className="intro-2">
+          Here, you will get to know more about me and what I do as a
+          {' '}
+          <span>Software Developer</span>
+        </p>
+      </div>
+      <div className="aboutme">
+        About me
+      </div>
+      <div className="skills">
+        About me
+      </div>
+      <div className="work">
+        About me
+      </div>
+      <div className="interests">
+        About me
+      </div>
+      <div className="contact">
         About me
       </div>
     </div>
