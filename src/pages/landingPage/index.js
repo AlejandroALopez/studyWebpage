7231fa68c8d4ba41383668b9e2b46596344b4aef
alejandro <lopez.alejandro@dali.dartmouth.edu>
2022-11-05 18:00:45 -0400

    added smooth scrolling

--- src/pages/landingPage/index.js
+++ src/pages/landingPage/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-scroll';
 // import { connect } from 'react-redux';
 import './style.scss';
 
@@ -10,28 +11,49 @@ export default function LandingPage(props) {
       <div className="landingPage-header">
         <div className="landingPage-header-name">Alejandro Lopez</div>
         <div className="landingPage-header-navigation">
-          <div>HOME</div>
-          <div>ABOUT ME</div>
-          <div>SKILLS</div>
-          <div>WORK</div>
-          <div>INTERESTS</div>
-          <div>CONTACT</div>
+          <div>
+            <Link to="homepage" spy smooth offset={50} duration={500}>HOME</Link>
+          </div>
+          <div>
+            <Link to="aboutme" spy smooth offset={50} duration={500}>ABOUT ME</Link>
+          </div>
+          <div>
+            <Link to="skills" spy smooth offset={50} duration={500}>HOME</Link>
+          </div>
+          <div>
+            <Link to="work" spy smooth offset={50} duration={500}>WORK</Link>
+          </div>
+          <div>
+            <Link to="interests" spy smooth offset={50} duration={500}>INTERESTS</Link>
+          </div>
+          <div>
+            <Link to="contact" spy smooth offset={50} duration={500}>CONTACT</Link>
+          </div>
         </div>
       </div>
-      <div className="homepage">
-        <p className="intro">
-          Hello! I am
-          {' '}
-          <span>Alejandro Lopez</span>
-          , and welcome to my personal website!
-        </p>
-        <p className="intro-2">
-          Here, you will get to know more about me and what I do as a
-          {' '}
-          <span>Software Developer</span>
-        </p>
+      <div className="homepage" id="homepage">
+        <img className="homepage-image" alt="profile_img" src="src/assets/profile.jpeg" />
+        <div className="intro-container">
+          <p className="intro">
+            Hello! I am
+            {' '}
+            <span>Alejandro Lopez</span>
+            {' '}
+            and I am a
+            {' '}
+            <span>Software Developer</span>
+          </p>
+          <div className="intro-buttons-container">
+            <button type="button" className="intro-button">
+              Contact me
+            </button>
+            <button type="button" className="intro-button">
+              My Resume
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="aboutme">
+      <div className="aboutme" id="aboutme">
         <div className="aboutme-info">
           <i className="fas fa-globe-americas fa-3x" />
           <p>Peru</p>
@@ -41,24 +63,24 @@ export default function LandingPage(props) {
           <p>Computer Science</p>
         </div>
       </div>
-      <div className="skills">
+      <div className="skills" id="skills">
         Skills
       </div>
-      <div className="work">
+      <div className="work" id="work">
         Work
       </div>
-      <div className="interests">
+      <div className="interests" id="interests">
         Interests
       </div>
-      <div className="contact">
+      <div className="contact" id="contact">
         <p className="contact-title">Contact info</p>
         <div className="contact-info">
           <i className="far fa-envelope fa-3x" />
-          <p>lopez.alejandro@dali.dartmouth.edu</p>
+          <p>alejandro.a.lopez.cochachi.23@dartmouth.edu</p>
         </div>
         <div className="contact-info">
           <i className="fas fa-mobile-alt fa-3x" />
-          <p>(+51) 957-605-311</p>
+          <p>(603) 277 - 8656</p>
         </div>
         <div className="contact-buttons">
           <i className="fab fa-linkedin fa-5x" />
