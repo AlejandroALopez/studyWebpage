e4aa6d9a3cfc29fbf694eaa579190dbc6ac0a52e
alejandro <lopez.alejandro@dali.dartmouth.edu>
2022-12-18 16:37:12 -0500

    contact icons working

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -4,18 +4,22 @@ import './style.scss';
 export default function Contact() {
   return (
     <div className="contact-container" id="contact">
-      <p className="contact-title">Contact info</p>
-      <div className="contact-info">
+      <p className="title">Contact info</p>
+      <div className="info-container">
         <i className="far fa-envelope fa-3x" />
         <p>alejandro.a.lopez.cochachi.23@dartmouth.edu</p>
       </div>
-      <div className="contact-info">
+      <div className="info-container">
         <i className="fas fa-mobile-alt fa-3x" />
         <p>(603) 277 - 8656</p>
       </div>
-      <div className="contact-buttons">
-        <i className="fab fa-linkedin fa-5x" />
-        <i className="fab fa-github fa-5x" />
+      <div className="buttons-container">
+        <a className="button" href="https://www.linkedin.com/in/alejandro-a-lopez/" target="_blank" rel="noreferrer">
+          <i className="fab fa-linkedin fa-8x" />
+        </a>
+        <a className="button" href="https://github.com/AlejandroALopez" target="_blank" rel="noreferrer">
+          <i className="fab fa-github fa-8x" />
+        </a>
       </div>
     </div>
   );
